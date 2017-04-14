0cb779d3b9bc3d018272d60b93cfbe22ca35b29d
sherpadance <sebastian@vermeersch-lanoye.com>
2017-04-15 01:17:37 +0200

    Code and output for plot 1

---
+++ plot1.R
@@ -0,0 +1,32 @@
+library(data.table)
+
+# Check if source data is present
+# download/unzip if not
+if(!file.exists("household_power_consumption.zip")) {
+    download.file(
+        "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+        "household_power_consumption.zip",
+        mode="wb"
+    )
+    unzip("household_power_consumption.zip", overwrite=TRUE)
+}
+
+# Read source data file
+data <- fread(
+    "household_power_consumption.txt",
+    sep = ";",
+    header = TRUE,
+    na.strings = "?"
+)
+
+# Convert dates to Date objects and subset data
+data$Date <- as.Date(strptime(data$Date, format="%d/%m/%Y"))
+data <- subset(data, Date==as.Date("2007-02-01")|Date==as.Date("2007-02-02"))
+
+# Create plot to png device
+png("plot1.png")
+hist(data$Global_active_power,
+     main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.off()
